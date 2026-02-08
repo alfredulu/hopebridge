47b046e45f78b962cf2933fa14582ac01a1e1752
Alfred <alfredulu3328@gmail.com>
2026-02-09 00:01:58 +0100

    make home page an independent file

---
+++ src/pages/InfoPage.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import CauseCard from "../components/CauseCard";
+import { HandHeart, TrendingUp, Smile, Heart } from "lucide-react";
